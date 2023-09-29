18fd0ca600fd6c5d0019ba56da33646e164cabed
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-09-29 17:39:41 +0530

    add api call example

--- app/script.js
+++ app/script.js
@@ -1,1 +1,25 @@
 // https://dog.ceo/api/breeds/image/random Fetch!
+async function getRandomDogImageFromApi() {
+  //     fetch("https://dog.ceo/api/breeds/image/random");
+
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((res) => res.json())
+  //     .then((json) => console.log(json));
+  try {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random").then(
+      (res) => res.json()
+    );
+    return res.message;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getImage() {
+  try {
+    const image = await getRandomDogImageFromApi();
+    console.log(image);
+  } catch (error) {
+    console.log(error);
+  }
+}
