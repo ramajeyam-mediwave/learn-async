b79c93ff33969830b5d72703fe2e6711b6f8d88d
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-03 10:14:30 +0530

    completed

--- app/main.js
+++ app/main.js
@@ -1,25 +1,67 @@
 import "./style.css";
-
 import { fetchRandomDogImage } from "./utils/dogApi";
+import { fetchAllDogList } from "./utils/dogApi";
 
-// import { animals } from "./utils/dog";
-// // import { allDogs } from "./utils/dog";
+function showLoading() {
+  const loadingElement = document.querySelector("#loading");
+  if (loadingElement) {
+    loadingElement.style.display = "block";
+  }
+}
 
-// function printDogs() {
-//   animals.sayHello();
-//   // console.log(animals);
-// }
+function hideLoading() {
+  const loadingElement = document.querySelector("#loading");
+  if (loadingElement) {
+    loadingElement.style.display = "none";
+  }
+}
 
-// document.querySelector("#app").innerHTML = `<h1>Hello world</h1>`;
-// printDogs();
-
-async function start() {
+async function fetchDogList() {
   try {
-    const res = await fetchRandomDogImage();
-    console.log(res.data.message);
+    const response = await fetchAllDogList();
+    const dogList = response.data.message;
+    const dogNames = Object.keys(dogList);
+    console.log(dogNames);
+    return dogNames;
   } catch (error) {
     console.log(error);
   }
 }
 
+async function displayRandomDogImage(dogBreed) {
+  try {
+    showLoading(); 
+
+    const res = await fetchRandomDogImage(dogBreed);
+    const dogImage = document.querySelector("#img");
+    dogImage.src = res.data.message;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoading(); 
+  }
+}
+
+async function start() {
+  const dogNamesList = await fetchDogList();
+  appendDogNamesToSelect(dogNamesList);
+}
+
+function appendDogNamesToSelect(dogNamesList) {
+  const selectBreed = document.querySelector("#dog-list");
+  for (let i = 0; i < dogNamesList.length; i++) {
+    const item = dogNamesList[i];
+    const option = document.createElement("option");
+    option.textContent = item;
+    selectBreed.appendChild(option);
+  }
+}
+
 start();
+
+document.querySelector("#button").addEventListener("click", function () {
+  const selectBreed = document.querySelector("#dog-list");
+  const dogImage = document.querySelector("#img");
+
+  displayRandomDogImage(selectBreed.value);
+});
--- app/utils/dogApi.js
+++ app/utils/dogApi.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const fetchRandomDogImage = () => {
-  return axios.get("https://dog.ceo/api/breeds/image/random");
+export const fetchRandomDogImage = (dog) => {
+  return axios.get(`https://dog.ceo/api/breed/${dog}/images/random`);
+};
+
+export const fetchAllDogList = () => {
+  return axios.get("https://dog.ceo/api/breeds/list/all");
 };
